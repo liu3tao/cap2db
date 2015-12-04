622e0826b08831114e3b9549f02a8a48fd31a049
Tao Liu <tao.liu@flukenetworks.com>
2015-12-04 13:59:15 -0800

    Update xbee capture with channel change

--- capSSID-xbee.py
+++ capSSID-xbee.py
@@ -2,7 +2,10 @@ import sys, csv, os, time
 import subprocess
 import urllib
 import serial
-import string, operator
+import string, operator, random
+
+def changeChannel(c, iface):
+    subprocess.call('iwconfig ' + iface + ' channel ' + str(c), shell=True)
 
 def isPrintable(s):
     return all(c in string.printable for c in s)
@@ -18,6 +21,7 @@ if len(sys.argv) != 2:
 
 PORT = '/dev/ttyUSB0'
 RATE = '115200'
+HOST_NAME = 'NB1: '
 ser = serial.Serial(PORT, RATE)
 if ser.isOpen():
     print 'Serial opened:', PORT, RATE
@@ -42,6 +46,9 @@ content = []
 ssidCnt = {}
 startT = time.time()
 lastReportT = time.time()
+currentChannel = 1
+changeChannel(currentChannel, sys.argv[1])
+
 for aline in run_command(tsharkCmd):
     alist = aline.strip().split(',')
     if len(alist) != len(tsharkPara):
@@ -49,7 +56,10 @@ for aline in run_command(tsharkCmd):
         continue
 
     ssid = alist[0]
-    subtype = int(alist[1])
+    try:
+        subtype = int(alist[1])
+    except ValueError:
+        continue
     if ssid != '' and isPrintable(ssid):
             if ssid in ssidCnt:
                 tmp = ssidCnt[ssid]
@@ -68,18 +78,23 @@ for aline in run_command(tsharkCmd):
             ssidCnt[ssid] = tmp
             #print ssid, ssidCnt[ssid]
     else:
-        print 'not printable SSID:', ssid
-    if (time.time() - lastReportT) > 10:
+        # print 'not printable SSID:', ssid
+        pass
+    if (time.time() - lastReportT) > 10 + random.randint(0,3):
         sortedCnt = sorted(ssidCnt.items(), key=operator.itemgetter(1,0), reverse=True)
+        ser.write(HOST_NAME + '=== CH: ' + str(currentChannel) + ' ===\n\r')
         for i in range(min(len(sortedCnt), 5)):
             (s,cnt) = sortedCnt[i]
-            ser.write('RPI: ' + s + '\t(' + str(cnt[0]) +',B:'+str(cnt[1]) +',Q:'+str(cnt[2]) +',R:'+str(cnt[3]) +',?:'+str(cnt[4]) +  ')\n\r')
-            print 'RPI: ' + s + '\t(' + str(cnt) + ')'
+            ser.write(HOST_NAME + s + '\t(' + str(cnt[0]) +',B:'+str(cnt[1]) +',Q:'+str(cnt[2]) +',R:'+str(cnt[3]) +',?:'+str(cnt[4]) +  ')\n\r')
+            print HOST_NAME + s + '\t(' + str(cnt) + ')'
             time.sleep(0.01)
         ssidCnt = {}
         lastReportT = time.time()
+        currentChannel += 1
+        if currentChannel > 14:
+            currentChannel = 1
+        changeChannel(currentChannel, sys.argv[1])
         print '======================================='
-        ser.write('RPI: ===================\n\r')
 print '=========='
 
 print 'All done!'
